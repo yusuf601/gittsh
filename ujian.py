32523df1f4632e4223225090e86b35ec8c518935
Yusuf Muhyusuh <yusufmuhyusuh@gmail.com>
2024-02-02 15:31:43 +0700

    belajar operator

---
+++ ujian.py
@@ -0,0 +1,23 @@
+def simpan_waktu(kedatangan, pulang):
+  """
+  Menyimpan waktu kedatangan dan pulang ke dalam dictionary.
+
+  Args:
+    kedatangan: Waktu kedatangan dalam format string.
+    pulang: Waktu pulang dalam format string.
+
+  Returns:
+    Dictionary yang berisi waktu kedatangan dan pulang.
+  """
+
+  # Buat dictionary kosong.
+  jadwal_datang = startTime = {"HH": 9, "mm": 23, "ss": 57,}
+  jadwal_pulang = endTime = {"HH": 15, "mm": "25", "ss": 43,}
+  # Masukkan waktu kedatangan ke dalam dictionary.
+
+
+
+  # Tampilkan dictionary.
+  print(jadwal_datang)
+
+

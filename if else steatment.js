86aa54dbe17ecadf84c38606e5f8e47fdb0c292b
Yusuf Muhyusuh <yusufmuhyusuh@gmail.com>
2024-02-02 15:59:19 +0700

    menambahkan ifelse

---
+++ if else steatment.js
@@ -0,0 +1,16 @@
+// If/Else Statement
+
+let x = 50;
+
+if(x > 60) {
+    console.log(x);
+} else {
+    console.log("Nilai kurang dari 70");
+}
+
+// TERNERARY OPERATIONS
+const isMember = false;
+const discount = isMember ? 0.1 : 0;
+console.log(`Anda mendapatkan diskon sebesar ${discount * 100 }%`)
+
+
